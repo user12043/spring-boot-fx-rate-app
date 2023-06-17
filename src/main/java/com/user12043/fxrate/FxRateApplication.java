a8296bcad39b32b671b3d135295e2eabe5f22201
user12043 <user12043@hotmail.com>
2023-06-17 16:03:26 +0300

    initial commit

---
+++ src/main/java/com/user12043/fxrate/FxRateApplication.java
@@ -0,0 +1,13 @@
+package com.user12043.fxrate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FxRateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FxRateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/user12043/fxrate/FxRateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.user12043.fxrate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FxRateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
