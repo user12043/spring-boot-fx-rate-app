3cd91d06eae5ac39e1689fbae4d06126a2e768df
user12043 <user12043@hotmail.com>
2023-06-19 04:43:19 +0300

    fix invalid usage of @Bean

--- src/main/java/com/user12043/fxrate/client/ExchangeAPI.java
+++ src/main/java/com/user12043/fxrate/client/ExchangeAPI.java
@@ -3,7 +3,6 @@ package com.user12043.fxrate.client;
 import com.user12043.fxrate.dto.external.ExchangeConvertResponse;
 import com.user12043.fxrate.dto.external.ExchangePairResponse;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.HttpClientErrorException;
@@ -11,22 +10,19 @@ import org.springframework.web.client.RestTemplate;
 
 @Component
 public class ExchangeAPI {
+    private final RestTemplate template = new RestTemplate();
+
     @Value("${exchange.api.basePath}")
     private String exchangeApiPath;
 
-    @Bean
-    private RestTemplate getTemplate() {
-        return new RestTemplate();
-    }
-
     public ExchangePairResponse fetchRateFromRemote(String baseCurrency, String targetCurrency) throws HttpClientErrorException {
-        final ResponseEntity<ExchangePairResponse> responseEntity = getTemplate().getForEntity(
+        final ResponseEntity<ExchangePairResponse> responseEntity = template.getForEntity(
                 "%s/pair/%s/%s".formatted(exchangeApiPath, baseCurrency, targetCurrency), ExchangePairResponse.class);
         return responseEntity.getBody();
     }
 
     public ExchangeConvertResponse fetchConversionFromRemote(String baseCurrency, String targetCurrency, double amount) throws HttpClientErrorException {
-        final ResponseEntity<ExchangeConvertResponse> responseEntity = getTemplate().getForEntity(
+        final ResponseEntity<ExchangeConvertResponse> responseEntity = template.getForEntity(
                 "%s/pair/%s/%s/%f".formatted(exchangeApiPath, baseCurrency, targetCurrency, amount), ExchangeConvertResponse.class);
         return responseEntity.getBody();
     }
